9fb3cf809a2dc6887f9b2fb91b8c3029408585e6
reddykarthik007 <132653506+reddykarthik007@users.noreply.github.com>
2023-05-06 13:41:52 +0530

    Add files via upload

---
+++ 12.printing digits numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+}
---
+++ 18.program to convert hexa to decimal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+  
+int main()
+{
+  
+    int n;
+    // taking input from user
+    scanf("%x", &n);
+    
+    // printing the decimal number
+    printf("%d", n);
+}
